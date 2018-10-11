4776e03fae8448006790f316dadf12cfe6801fe0
Matthew Cowie <matthew@cowie.me>
2018-10-11 01:50:18 -0500

    Organize package names (and add a few for good measure)

--- Windows/AppInstaller.ps1
+++ Windows/AppInstaller.ps1
@@ -4,37 +4,54 @@
 # Failing that, use Chocolatey Package Manager
 
 Write-Output "Installing Chocolatey"
-Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 Write-Output "Installing Browsers"
 choco install firefox chromium -y
 
-Write-Output "Installing Utilties"
-choco install 1password -y
-choco install peazip -y
-choco install vlc -y
-choco install mpv -y
-choco install youtube-dl -y
-choco install rufus -y
-choco install digikam -y
-choco install retroarch -y
+$utilities = @(
+    "1password"
+    "peazip.install"
+    "windirstat"
+    "qbittorrent"
+    "rufus"
+)
 
-# Extras
-# choco install calibre
-# choco install picard
-# choco install handbrake.install
-# choco install obs-studio
+$media_tools = @(
+    "vlc"
+    "mpv"
+    "youtube-dl"
+    "audacity"
+    "digikam"
+    "retroarch"
+    # "calibre"
+    # "picard"
+    # "handbrake.install"
+    # "obs-studio"
+)
+
+Write-Output "Installing Utilties"
+$utilities | ForEach-Object{choco install $_ --force -y}
+
+Write-Output "Installing Multimedia Tools"
+$media_tools | ForEach-Object{choco install $_ --force -y}
 
 # Most dev tools live in WSL, but certain tools must (or are improved by)
 # natively running in Windows for various reasons
 # e.g GUI, filesystem interop, Selenium drivers for native browsers
+
+$dev_tools = @(
+    "SublimeText3"
+    "vscode"
+    "heroku-cli"
+    "selenium-gecko-driver"
+    "selenium-chrome-driver"
+    "selenium-edge-driver"
+    "selenium-ie-driver"
+)
+
 Write-Output "Installing Host OS Dev Tools"
-choco install SublimeText3 -y
-choco install vscode -y
 choco install git.install --params="/NoShellIntegration /GitAndUnixToolsOnPath /SChannel" -y
-choco install heroku-cli -y
-choco install selenium-gecko-driver -y
-choco install selenium-chrome-driver -y
-choco install selenium-edge-driver -y
-choco install selenium-ie-driver -y
+$dev_tools | ForEach-Object{choco install $_ --force -y}
+
 
