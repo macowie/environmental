0a570644f1dbbeca5bbeaddb8a894fe1b209afa9
Matthew Cowie <matthew@cowie.me>
2018-10-11 01:53:57 -0500

    Convert copypasta CMDs to powershell

--- Windows/MiscPreferences.ps1
+++ Windows/MiscPreferences.ps1
@@ -1,23 +1,20 @@
-# Miscellaneous
+# Miscellaneous Prefs
 
-
-Write-Output "Setting default explorer view to This PC"
+Write-Output "Explorer: Setting default view to This PC"
 Set-ItemProperty "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" "LaunchTo" 1
 
-REM *** Show hidden files in Explorer ***
-REM reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "Hidden" /t REG_DWORD /d 1 /f
+Write-Output "Explorer: Show file extensions"
+Set-ItemProperty "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" "HideFileExt" 0
 
-REM *** Show super hidden system files in Explorer ***
-REM reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "ShowSuperHidden" /t REG_DWORD /d 1 /f
+# Write-Output "Explorer: Show hidden files"
+# Set-ItemProperty "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" "Hidden" 1
 
-REM *** Show file extensions in Explorer ***
-reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "HideFileExt" /t  REG_DWORD /d 0 /f
+# Write-Output "Explorer: Show super hidden files"
+# Set-ItemProperty "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" "ShowSuperHidden" 1
 
-REM *** Hide the search box from taskbar. You can still search by pressing the Win key and start typing what you're looking for ***
-REM 0 = hide completely, 1 = show only icon, 2 = show long search box
-reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" /v "SearchboxTaskbarMode" /t REG_DWORD /d 0 /f
-
-
+Write-Output "Shell: hide taskbar search"
+# 0 = hide completely, 1 = show only icon, 2 = show long search box
+Set-ItemProperty "HKCU:\Software\Microsoft\Windows\CurrentVersion\Search" "SearchboxTaskbarMode" 0
 
 Write-Out "Enable Windows Subsystem for Linux (reboot required)"
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
