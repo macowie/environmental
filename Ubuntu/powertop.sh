cd6c08425f9f2ea05805ceb1e0e2e2db69b62a73
Matthew Cowie <matthew@cowie.me>
2018-11-06 10:44:08 -0600

    Add powertop auto tuning

---
+++ Ubuntu/powertop.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Installs Intel PowerTop and adds a service to autotune every boot
+sudo apt install powertop
+
+cat << EOF | sudo tee /etc/systemd/system/powertop.service
+[Unit]
+Description=PowerTOP auto tune
+
+[Service]
+Type=idle
+Environment="TERM=dumb"
+ExecStart=/usr/sbin/powertop --auto-tune
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reload
+systemctl enable powertop.service
