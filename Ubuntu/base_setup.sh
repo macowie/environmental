e94f2d474ebac9affaa73c123e22690ff6840499
Matthew Cowie <matthew@cowie.me>
2018-11-19 22:27:04 -0600

    Add chromedriver setup

--- Ubuntu/base_setup.sh
+++ Ubuntu/base_setup.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash 
+#!/usr/bin/env bash
 
 # Upgrade base packages
 sudo apt update
@@ -9,6 +9,8 @@ sudo apt install -y autoconf bison build-essential libssl-dev libyaml-dev \
                     libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev \
                     libgdbm5 libgdbm-dev libsqlite3-dev
 
+sudo apt install -y chromium-chromedriver
+ln -s /usr/lib/chromium-browser/chromedriver /usr/bin/chromedriver
 # Install multimedia libraries
 sudo apt install -y ffmpeg libvips imagemagick mupdf
 
