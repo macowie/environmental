31aad3dc3828c38d05d67a3035b7081f479e442a
Matthew Cowie <matthew@cowie.me>
2019-01-07 00:58:14 -0600

    Fix sudo in tlp setup

--- Linux/Fedora/tlp.sh
+++ Linux/Fedora/tlp.sh
@@ -3,13 +3,13 @@
 # Install TLP Advance Power Management
 # https://linrunner.de/en/tlp/docs/tlp-linux-advanced-power-management.html
 
-dnf install -y tlp tlp-rdw 
+sudo dnf install -y tlp tlp-rdw 
  
 read -p "Install ThinkPad battery extensions? " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-   dnf install -y https://repo.linrunner.de/fedora/tlp/repos/releases/tlp-release.fc$(rpm -E %fedora).noarch.rpm 
-   dnf install -y akmod-tp_smapi akmod-acpi_call kernel-devel   
+   sudo dnf install -y https://repo.linrunner.de/fedora/tlp/repos/releases/tlp-release.fc$(rpm -E %fedora).noarch.rpm 
+   sudo dnf install -y akmod-tp_smapi akmod-acpi_call kernel-devel   
 fi
 
 tlp start
