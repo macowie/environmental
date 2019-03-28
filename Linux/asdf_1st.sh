20ed10301de4a04c39a61c7a5196607f4f79f27b
Matthew Cowie <matthew@cowie.me>
2019-03-27 20:17:47 -0500

    Use .bash_profle instead of .profile

--- Linux/asdf_1st.sh
+++ Linux/asdf_1st.sh
@@ -8,10 +8,12 @@ cd ~/.asdf
 git checkout "$(git describe --abbrev=0 --tags)"
 
 # Activate asdf in bash & fish
-append_if_not_present ~/.profile 'source $HOME/.asdf/asdf.sh'
+append_if_not_present ~/.bash_profile 'source $HOME/.asdf/asdf.sh'
 append_if_not_present ~/.bashrc 'source $HOME/.asdf/completions/asdf.bash'
 append_if_not_present ~/.config/fish/config.fish 'source ~/.asdf/asdf.fish'
 mkdir -p ~/.config/fish/completions && cp ~/.asdf/completions/asdf.fish ~/.config/fish/completions
 
 # Enable .ruby-version, .node-version, etc files
 echo "legacy_version_file = yes" > $HOME/.asdfrc
+
+echo "** asdf-vm is now installed. Please open a new terminal to proceed. **"
