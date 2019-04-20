7131583f53d30e3577a5901e5345b44cfff04fa0
Matthew Cowie <matthew@cowie.me>
2019-04-20 02:26:14 +0000

    Reorg profile editing order

--- Linux/asdf_1st.sh
+++ Linux/asdf_1st.sh
@@ -8,10 +8,13 @@ cd ~/.asdf
 git checkout "$(git describe --abbrev=0 --tags)"
 
 # Activate asdf in bash & fish
+mkdir -p ~/.config/fish/
+touch ~/.bash_profile ~/.bashrc ~/.config/fish/config.fish
+mkdir -p ~/.config/fish/
+mkdir -p ~/.config/fish/completions && cp ~/.asdf/completions/asdf.fish ~/.config/fish/completions
 append_if_not_present ~/.bash_profile 'source $HOME/.asdf/asdf.sh'
 append_if_not_present ~/.bashrc 'source $HOME/.asdf/completions/asdf.bash'
 append_if_not_present ~/.config/fish/config.fish 'source ~/.asdf/asdf.fish'
-mkdir -p ~/.config/fish/completions && cp ~/.asdf/completions/asdf.fish ~/.config/fish/completions
 
 # Enable .ruby-version, .node-version, etc files
 echo "legacy_version_file = yes" > $HOME/.asdfrc
