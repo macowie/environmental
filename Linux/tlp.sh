4c44e10c83a9d395747672437bd2a4c0d7b33028
Matthew Cowie <matthew@cowie.me>
2019-04-30 15:52:47 -0500

    Syntax fixes for TLP script

--- Linux/tlp.sh
+++ Linux/tlp.sh
@@ -8,14 +8,14 @@ source ./utils/utils.sh
 read THINKPAD -p "Install ThinkPad battery extensions? " -n 1 -r
 echo
 
-if is_fedora; then
+if [ is_fedora ]; then
   sudo dnf install -y tlp tlp-rdw
 
   if [[ $THINKPAD =~ ^[Yy]$ ]]; then
     sudo dnf install -y https://repo.linrunner.de/fedora/tlp/repos/releases/tlp-release.fc$(rpm -E %fedora).noarch.rpm
     sudo dnf install -y akmod-tp_smapi akmod-acpi_call kernel-devel
   fi
-elif is_ubuntuy; then
+elif [ is_ubuntuy ]; then
   sudo add-apt-repository ppa:linrunner/tlp
   sudo apt update
   sudo apt install tlp tlp-rdw
